acf3909d691fbd1135e940d5e7852fef41f35e4f
SouravRaj32 <65517504+SouravRaj32@users.noreply.github.com>
2023-04-02 08:36:24 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -99,7 +99,7 @@ const Header = ({ type }) => {
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
                 <input
                   type="text"
-                  placeholder="Where are you going?"
+                  placeholder="Enter Berlin, Madrid or London"
                   className="headerSearchInput"
                   onChange={(e) => setDestination(e.target.value)}
                 />
